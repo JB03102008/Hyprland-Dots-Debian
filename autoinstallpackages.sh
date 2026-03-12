9b9748a749cfc22a8477d714a8ef81c071828240
JB03102008 <jasper_bloemendal@icloud.com>
2026-03-12 19:14:07 +0100

    Update autoinstallpackages.sh

--- autoinstallpackages.sh
+++ autoinstallpackages.sh
@@ -13,7 +13,7 @@ fix_libreoffice_scaling() {
     for file in /usr/share/applications/libreoffice-*.desktop; do
         if [ -f "$file" ]; then
             echo "Patching $file"
-            sudo sed -i 's|Exec=libreoffice|Exec=env SAL_FORCEDPI=192 libreoffice|g' "$file"
+	   sudo sed -i 's|^Exec=\(.*\)|Exec=env SAL_FORCEDPI=144 XCURSOR_SIZE=24 \1|' /usr/share/applications/libreoffice-*.desktop
         fi
     done
     sudo update-desktop-database /usr/share/applications
